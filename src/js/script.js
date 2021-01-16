13bdd46302418eefb8b0be9dbe3319d9f82abf46
paweljankowski <paweljankowski92@gmail.com>
2021-01-16 15:57:20 +0100

    modul 8.4 8.5 act

--- src/js/script.js
+++ src/js/script.js
@@ -227,38 +227,38 @@
     prepareCartProduct() {
       const thisProduct = this;
 
-      const productSummary = {id: thisProduct.id, name: thisProduct.data.name, amount: thisProduct.amountWidget.value, priceSingle: thisProduct.priceSingle, price: priceSingle *= amount, params: {} };
-
+      // const productSummary = {id: thisProduct.id, name: thisProduct.data.name, amount: thisProduct.amountWidget.value, priceSingle: thisProduct.priceSingle, price: priceSingle *= amount, params: {} };
+      //
       return productSummary;
 
     }
 
     prepareCartProductParams () {
-        const thisProduct = this;
-        // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
-        const formData = utils.serializeFormToObject(thisProduct.form);
-        // set price to default price
-        const params = {};
-        // for every category (param)...
-        for(let paramId in thisProduct.data.params) {
-          // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
-          const param = thisProduct.data.params[paramId];
+      const thisProduct = this;
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      // set price to default price
+      const params = {};
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
 
-          params[paramId] = {
-            name: param.label,
-            options: {}
-          }
-          // for every option in this category
-          for(let optionId in param.options) {
-            // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
-            const option = param.options[optionId];
-            const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
-            // const optionsSelected = formData.hasOwnProperty;
-            if (formData[paramId] && formData[paramId].includes(optionId)) {
-              if(optionSelected) {
-
-              }
-          }
+        params[paramId] = {
+          name: param.label,
+          options: {}
+        };
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+          // const optionsSelected = formData.hasOwnProperty;
+          if (formData[paramId] && formData[paramId].includes(optionId)) {
+          //   if(optionSelected) {
+          //
+          //    }
+          // }
         }
 
 
@@ -366,38 +366,38 @@
     // }
   }
 
-// class CartProduct{
-//   constructor(menuProduct, element) {
-//     const thisCartProduct = this;
-//     thisCartProduct.id = menuProduct.id;
-//
-//     initCartProduct();
-//     getElements(element);
-//
-//     console.log('thisCartProduct', thisCartProduct);
-//
-//
-//   }
-//
-//   getElements(element) {
-//     const thisCartProduct = this;
-//     thisCartProduct.dom = {};
-//
-//     thisCartProduct.dom.wrapper = element;
-//     thisCartProduct.dom.amountWidget = thisCartProduct.element.querySelector(select.cartProduct.amountWidget);
-//     thisCartProduct.dom.price = thisCartProduct.element.querySelector(select.cartProduct.price);
-//     thisCartProduct.dom.edit = thisCartProduct.element.querySelector(select.cartProduct.edit);
-//     thisCartProduct.dom.remove = thisCartProduct.element.querySelector(select.cartProduct.remove);
-//
-//   }
-//   initCartProduct() {
-//     const thisCartProduct = this;
-//     thisCartProduct.initCartProduct = new initCartProduct(thisProduct.initCartProduct);
-//     thisCartProduct.amountWidgetElem.addEventListener('click', function() {
-//
-//     });
-//   }
-// }
+  // class CartProduct{
+  //   constructor(menuProduct, element) {
+  //     const thisCartProduct = this;
+  //     thisCartProduct.id = menuProduct.id;
+  //
+  //     initCartProduct();
+  //     getElements(element);
+  //
+  //     console.log('thisCartProduct', thisCartProduct);
+  //
+  //
+  //   }
+  //
+  //   getElements(element) {
+  //     const thisCartProduct = this;
+  //     thisCartProduct.dom = {};
+  //
+  //     thisCartProduct.dom.wrapper = element;
+  //     thisCartProduct.dom.amountWidget = thisCartProduct.element.querySelector(select.cartProduct.amountWidget);
+  //     thisCartProduct.dom.price = thisCartProduct.element.querySelector(select.cartProduct.price);
+  //     thisCartProduct.dom.edit = thisCartProduct.element.querySelector(select.cartProduct.edit);
+  //     thisCartProduct.dom.remove = thisCartProduct.element.querySelector(select.cartProduct.remove);
+  //
+  //   }
+  //   initCartProduct() {
+  //     const thisCartProduct = this;
+  //     thisCartProduct.initCartProduct = new initCartProduct(thisProduct.initCartProduct);
+  //     thisCartProduct.amountWidgetElem.addEventListener('click', function() {
+  //
+  //     });
+  //   }
+  // }
 
 
 
