9c374e509ea81da98824df656c88b58ecae18ba4
Paweł Jankowski <paweljankowski92@gmail.com>
2021-02-02 16:10:16 +0100

    modul 10.1 date-hourpicker

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,5 +1,7 @@
 import {select, templates} from '../settings.js';
 import AmountWidget from './AmountWidget.js';
+import DatePicker from './DatePicker.js';
+import HourPicker from './HourPicker.js';
 
 class Booking {
   constructor(element){
@@ -19,6 +21,8 @@ class Booking {
     thisBooking.dom.wrapper.innerHTML = generatedHTML;
     thisBooking.dom.peopleAmount = document.querySelector(select.booking.peopleAmount);
     thisBooking.dom.hoursAmount = document.querySelector(select.booking.hoursAmount);
+    thisBooking.dom.datePicker = document.querySelector(select.widgets.datePicker.wrapper);
+    thisBooking.dom.hourPicker = document.querySelector(select.widgets.hourPicker.wrapper);
   }
 
   initWidgets(){
@@ -36,6 +40,19 @@ class Booking {
 
     });
 
+    thisBooking.datePicker = new DatePicker(thisBooking.dom.datePicker);
+    console.log('datePicker', thisBooking.datePicker);
+    
+    thisBooking.dom.datePicker.addEventListener('click', function() {
+
+    });
+
+    thisBooking.hourPicker = new HourPicker(thisBooking.dom.hourPicker);
+    console.log('hourPicker', thisBooking.hourPicker);
+
+    thisBooking.dom.hourPicker.addEventListener('click', function() {
+
+    });
   }
 
 }
